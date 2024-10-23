268c50abe7dd707b6b79059449a945416d90406b
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2024-10-23 22:23:59 +0300

    Пункт 4_2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,30 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Страница чая с ванилью и клюквой*/
+router.get('/vanilla', function(req, res, next) {
+  res.send("<h1>Чай с ванилью и клюквой</h1>")
+  });
+
+  /* Страница чая с лаймом*/
+router.get('/spirit', function(req, res, next) {
+  res.send("<h1>Чай с лаймом</h1>")
+  });
+
+  /* Страница чая со сливой*/
+router.get('/plum', function(req, res, next) {
+  res.send("<h1>Чай со сливой</h1>")
+  });
+
+  /* Страница чая с виноградом*/
+router.get('/grape', function(req, res, next) {
+  res.send("<h1>Чай с виноградом</h1>")
+  });
+
+  /* Страница Чая с черникой*/
+router.get('/blueberry', function(req, res, next) {
+  res.send("<h1>Чай с черникой</h1>")
+  });
+  
+
 module.exports = router;
