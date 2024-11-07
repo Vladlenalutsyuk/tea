4d90c972dfb0d0014b409d240b281211f1b21879
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2024-11-07 11:09:57 +0300

    tea_7_2

--- testMongoose.js
+++ testMongoose.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-const Tea = mongoose.model('Tea', { name: String });
+var schema = mongoose.Schema({ name: String })
+schema.methods.tey = function(){
+console.log(this.name + ", чай готов!")
+}
+const Tea = mongoose.model('Tea', schema);
 const tee = new Tea({ name: 'Чаёк' });
-tee.save().then(() => console.log('Чай готов!'));
+tee.save().then(() => tee.tey());
