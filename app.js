45b653c63fa5388744275c35a19b0ec67b18c062
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2024-11-07 11:41:04 +0300

    tea_8_1

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/tea2024')
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
