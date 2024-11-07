77937e6874d9a5993fdb8dde89bb8fb7c589625d
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2024-11-07 12:00:59 +0300

    tea_8_3

--- routes/teas.js
+++ routes/teas.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var Tea = require('../models/tea').Tea;
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с cats');
 });
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
+
+router.get("/:nick", async function(req, res, next) {
+    var teas = await Tea.find({nick: req.params.nick});
+    console.log(teas)
+    if(!teas.length) return next(new Error("Нет в наличии"))
+    var tea = teas[0];
+    res.render('tea', {
+    title: tea.title,
+    picture: tea.avatar,
+    desc: tea.desc
+    })
     });
+    
     
 
 module.exports = router;
