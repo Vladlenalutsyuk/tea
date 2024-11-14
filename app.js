340523cd79c4047e2aa73fa25dcca408d10ff741
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2024-11-14 12:25:20 +0300

    tea_9_1

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/tea2024')
+var session = require("express-session")
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,6 +24,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: "Tea",
+  cookie:{maxAge:60*1000},
+  proxy: true,
+  resave: true,
+  saveUninitialized: true
+  }))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
