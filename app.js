cc4adb9069e5767dd255ad8fd0a681d2b6f2d346
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2024-11-21 12:34:19 +0300

    tea_9_4

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var teas = require('./routes/teas');
 var app = express();
 
 // view engine setup
-app.engine('ejs',require('ejs-locals'));
+app.engine('ejs', require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -28,32 +28,40 @@ app.use(express.static(path.join(__dirname, 'public')));
 var MongoStore = require('connect-mongo');
 app.use(session({
   secret: "Tea",
-  cookie:{maxAge:60*1000},
+  cookie: { maxAge: 60 * 1000 },
   proxy: true,
   resave: true,
   saveUninitialized: true,
-  store: MongoStore.create({mongoUrl:
-    'mongodb://localhost/tea2024'})
-  }))
+  store: MongoStore.create({
+    mongoUrl:
+      'mongodb://localhost/tea2024'
+  })
+}))
+
+
+app.use(function (req, res, next) {
+  req.session.counter = req.session.counter + 1 || 1
+  next()
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/teas', teas);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error', {title: 'tea'});
+  res.render('error', { title: 'tea' });
 });
 
 module.exports = app;
